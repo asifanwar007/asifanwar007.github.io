acae657c0b3e440e7ad1302a7f7a599dd36c54ef
Asif <asifanwar0000@outlook.com>
2020-04-12 20:50:56 +0530

    iit delhi icon added

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function( $ ) {
     interval: 3500
   });
 
-  // JavaScript Chart
+  // Java Chart
   var doughnutData = [{
       value: 70,
       color: "#1abc9c"
@@ -28,21 +28,9 @@ jQuery(document).ready(function( $ ) {
       color: "#ecf0f1"
     }
   ];
-  var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+  var myDoughnut = new Chart(document.getElementById("java").getContext("2d")).Doughnut(doughnutData);
 
-  // Bootstrap Chart
-  var doughnutData = [{
-    value: 90,
-    color: "#1abc9c"
-  },
-  {
-    value: 10,
-    color: "#ecf0f1"
-  }
-  ];
-  var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
-
-  // WordPress Chart
+  // Pyhton Chart
   var doughnutData = [{
     value: 65,
     color: "#1abc9c"
@@ -52,9 +40,21 @@ jQuery(document).ready(function( $ ) {
     color: "#ecf0f1"
   }
   ];
-  var myDoughnut = new Chart(document.getElementById("wordpress").getContext("2d")).Doughnut(doughnutData);
+  var myDoughnut = new Chart(document.getElementById("python").getContext("2d")).Doughnut(doughnutData);
 
-  // HTML Chart
+  // CPP Chart
+  var doughnutData = [{
+    value: 50,
+    color: "#1abc9c"
+  },
+  {
+    value: 50,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("cpp").getContext("2d")).Doughnut(doughnutData);
+
+  // PSQL Chart
   var doughnutData = [{
     value: 80,
     color: "#1abc9c"
@@ -64,29 +64,29 @@ jQuery(document).ready(function( $ ) {
     color: "#ecf0f1"
   }
   ];
-  var myDoughnut = new Chart(document.getElementById("html").getContext("2d")).Doughnut(doughnutData);
+  var myDoughnut = new Chart(document.getElementById("psql").getContext("2d")).Doughnut(doughnutData);
 
-  // Photoshop Chart
-  var doughnutData = [{
-    value: 70,
-    color: "#1abc9c"
-  },
-  {
-    value: 30,
-    color: "#ecf0f1"
-  }
-  ];
-  var myDoughnut = new Chart(document.getElementById("photoshop").getContext("2d")).Doughnut(doughnutData);
+  // // Photoshop Chart
+  // var doughnutData = [{
+  //   value: 90,
+  //   color: "#1abc9c"
+  // },
+  // {
+  //   value: 10,
+  //   color: "#ecf0f1"
+  // }
+  // ];
+  // var myDoughnut = new Chart(document.getElementById("photoshop").getContext("2d")).Doughnut(doughnutData);
 
-  // Illustrator Chart
-  var doughnutData = [{
-    value: 50,
-    color: "#1abc9c"
-  },
-  {
-    value: 50,
-    color: "#ecf0f1"
-  }
-  ];
-  var myDoughnut = new Chart(document.getElementById("illustrator").getContext("2d")).Doughnut(doughnutData);
+  // // Illustrator Chart
+  // var doughnutData = [{
+  //   value: 50,
+  //   color: "#1abc9c"
+  // },
+  // {
+  //   value: 50,
+  //   color: "#ecf0f1"
+  // }
+  // ];
+  // var myDoughnut = new Chart(document.getElementById("illustrator").getContext("2d")).Doughnut(doughnutData);
 });
